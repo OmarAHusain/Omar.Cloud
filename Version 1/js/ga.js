a08bfb9906e3fb068fd116b14b90991d78a37c4e
Omar Husain <omar.a.husain@gmail.com>
2022-01-23 23:53:45 +1100

    init commit

--- Version 1/js/ga.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-// Google Analytics Script
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-162856487-1');
--- Version 1/js/header.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-// Header and Footer script to load html docs
-$(function(){
-	$("#home").load("../common/home.html");
-	$("#header").load("common/header.html"); 
-	$("#footer").load("common/footer.html");
-});
